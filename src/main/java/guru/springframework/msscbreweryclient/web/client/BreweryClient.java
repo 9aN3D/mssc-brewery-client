80adec73e950a63ba0bb9bafb3c9ce794022a9d2
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-01 16:37:22 +0100

    Add rest template

--- src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -1,16 +1,28 @@
 package guru.springframework.msscbreweryclient.web.client;
 
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Setter
 @Component
+@RequiredArgsConstructor
 @ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
 public class BreweryClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer";
 
     private String apiHost;
+
+    private final RestTemplate restTemplate;
+
+    public BeerDto getBeerById(UUID id) {
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + id.toString(), BeerDto.class);
+    }
 
 }
