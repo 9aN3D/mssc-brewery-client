96629b73cd89f6b46242b54d23f62ac500bb8a34
Volodymyr Kovalchuk <v.kovalchuk.n@gmail.com>
2019-11-01 17:02:32 +0100

    Add post with restTemplate

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,15 @@ class BreweryClientTest {
         BeerDto beer = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(beer);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        BeerDto beer = BeerDto.builder().beerName("New beer test").build();
+        URI uri = client.saveNewBeer(beer);
+
+        assertNotNull(uri);
+
+        System.out.println(uri);
     }
 }
